e3c900b9439e7e39956ce0e896f11d77831546e6
Sameer Sharma <sameercr2000@gmail.com>
2025-10-15 22:21:23 +0530

    Updated 2D Array Program

--- src/Ex_016_Arrays/Lab_164_2DArrayLoopPrint.java
+++ src/Ex_016_Arrays/Lab_164_2DArrayLoopPrint.java
@@ -8,12 +8,18 @@ public class Lab_164_2DArrayLoopPrint {
                 {7,8,9}
         };
 
+//        for (int i = 0; i < arr3_3.length; i++) {
+//            for (int j = 0; j < arr3_3[i].length; j++) {
+//                System.out.print(arr3_3[i][j]+ " | ");
+//                //System.out.println("*");
+//            }
+//            //System.out.print("\n");
+//            System.out.println();
+//        }
         for (int i = 0; i < arr3_3.length; i++) {
-            for (int j = 0; j < arr3_3[i].length; j++) {
-                System.out.print(arr3_3[i][j]+ " | ");
-                //System.out.println("*");
+            for (int j = 0; j < arr3_3.length; j++) {
+                System.out.print(arr3_3[i][j]);
             }
-            //System.out.print("\n");
             System.out.println();
         }
     }
